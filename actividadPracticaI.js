beab40de62970acb9917c28fbcbae99dc6f284be
FranciscoJoseBonet Desktop <franciscojosebonet@gmail.com>
2025-06-27 17:58:29 -0300

    Primera consigna realizada (Generacion de clase ProductManager y sus metodos)

---
+++ actividadPracticaI.js
@@ -0,0 +1,54 @@
+class ProductManager {
+	constructor() {
+		this.products = [];
+		this.elementsCount = 0;
+	}
+
+	_isCodeRepeated(_code) {
+		return this.products.some((product) => product.code === _code);
+	}
+
+	addProduct(_title, _description, _price, _thumbnail, _code, _stock) {
+		try {
+			if (this._isCodeRepeated(_code)) throw new Error("Code repeated");
+			if (!_title || !_description || !_price || !_thumbnail || !_code || !_stock) {
+				throw new Error("All the fields are required");
+			}
+
+			const product = {
+				title: _title,
+				description: _description,
+				price: _price,
+				thumbnail: _thumbnail,
+				code: _code,
+				stock: _stock,
+				id: this.elementsCount + 1,
+			};
+
+			this.products.push(product);
+			this.elementsCount++;
+		} catch (e) {
+			console.error(e);
+			return false;
+		}
+
+		return true;
+	}
+
+	getProducts() {
+		this.products.map((prod) => {
+			console.log(`=====================================`);
+			console.log(`PRODUCT ID: ${prod.id}`);
+			console.log(`PRODUCT TITLE: ${prod.title}`);
+			console.log(`PRODUCT DESCRIPTION: ${prod.description}`);
+			console.log(`PRODUCT PRICE: ${prod.price}`);
+			console.log(`PRODUCT CODE: ${prod.code}`);
+			console.log(`PRODUCT STOCK: ${prod.stock}`);
+		});
+	}
+
+	getProductById(_id) {
+		const prodTargeted = this.products.find((prod) => prod.id === _id);
+		return prodTargeted ? prodTargeted : "Not Found";
+	}
+}
